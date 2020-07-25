f1641896072a8d53d2448625b64e05f04a398f7d
rjtt17 <rjtt17@gmail.com>
2020-07-25 14:22:18 +0900

    コメント頂いた内容を修正

--- memo-app.rb
+++ memo-app.rb
@@ -5,17 +5,20 @@ require "sinatra/reloader"
 require "json"
 
 get "/top" do
-  unless File.exist?("memo.json") then Memo.save("memo.json", { "memos": [] }) end
-  @memo_array = Memo.read("memo.json").array
+  if File.exist?("memo.json")
+    @memos = Memo.read("memo.json").array
+  else
+    Memo.save("memo.json", { "memos": [] })
+    @memos = Memo.read("memo.json").array
+  end
   erb :top
 end
 
 post "/top" do
-  p params
-  post_title = params["title"]
-  post_body = params["body"].gsub(/[\r]/, "").split("\n")
+  title = params["title"]
+  body = params["body"].gsub(/[\r]/, "").split("\n")
   memo = Memo.read("memo.json")
-  Memo.save("memo.json", memo.add_memo(memo, memo.array.size, post_title, post_body))
+  Memo.save("memo.json", memo.add_memo(memo.array.size, title, body))
   redirect "/top"
 end
 
@@ -26,32 +29,30 @@ end
 get "/show/:id" do
   number = params[:id].to_i
   memo = Memo.read("memo.json")
-  @url_edit, @url_top_id, @url_top  = "/edit/#{number}", "/show/#{number}", "/top"
-  @title, @body = memo.fetch_title(number - 1), memo.fetch_body(number - 1)
+  @title, @body , @number = memo.title(number - 1), memo.body(number - 1), number.to_s
   erb :show
 end
 
 get "/edit/:id" do
   number = params[:id].to_i
   memo = Memo.read("memo.json")
-  @url = "/edit/#{number}"
-  @title, @body = memo.fetch_title(number - 1), memo.fetch_body(number - 1).gsub(/<br>/, "\n")
+  @title, @body, @number = memo.title(number - 1), memo.body(number - 1).gsub(/<br>/, "\n"), number.to_s
   erb :edit
 end
 
 delete "/show/:id" do
   number = params[:id].to_i
   memo = Memo.read("memo.json")
-  Memo.save("memo.json", memo.delete_memo(memo, number))
+  Memo.save("memo.json", memo.delete_memo(number))
   redirect "/top"
 end
 
 patch "/edit/:id" do
   number = params[:id].to_i
-  update_title = params["title"]
-  update_body = params["body"].gsub(/[\r]/, "").split("\n")
+  title = params["title"]
+  body = params["body"].gsub(/[\r]/, "").split("\n")
   memo = Memo.read("memo.json")
-  Memo.save("memo.json", memo.update_memo(memo, number, update_title, update_body))
+  Memo.save("memo.json", memo.update_memo(number, title, body))
   redirect "/top"
 end
 
@@ -74,34 +75,31 @@ class Memo
     @array = hash["memos"]
   end
 
-  def fetch_id(number)
+  def id(number)
     @array[number]["id"]
   end
 
-  def fetch_title(number)
+  def title(number)
     @array[number]["title"]
   end
 
-  def fetch_body(number)
+  def body(number)
     @array[number]["body"]
   end
 
-  def add_memo(instance, id, title, body)
-    memo_hash = instance.hash
-    memo_hash["memos"].append({ id: id + 1, title: title, body: body.join("<br>") })
-    memo_hash
+  def add_memo(id, title, body)
+    @hash["memos"].append({ id: id + 1, title: title, body: body.join("<br>") })
+    @hash
   end
 
-  def update_memo(instance, id, title, body)
-    memo_hash = instance.hash
-    memo_hash["memos"][id - 1] = { id: id, title: title, body: body.join("<br>") }
-    memo_hash
+  def update_memo(id, title, body)
+    @hash["memos"][id - 1] = { id: id, title: title, body: body.join("<br>") }
+    @hash
   end
 
-  def delete_memo(instance, id)
-    memo_hash = instance.hash
-    memo_hash["memos"].delete_at(id - 1)
-    memo_hash["memos"].map.with_index(1) { |hash, index| hash["id"] = index }
-    memo_hash
+  def delete_memo(id)
+    @hash["memos"].delete_at(id - 1)
+    @hash["memos"].map.with_index(1) { |hash, index| hash["id"] = index }
+    @hash
   end
 end
